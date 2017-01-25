2200e13e274a89dd8dd17ab3d5eca51ac567d462
Albert <onionxu@gmail.com>
2017-01-25 15:00:18 +0800

    tried add 'Edit' founction in 'My Posts' page, but no lucky

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,23 +2,46 @@ class PostsController < ApplicationController
 
 	before_filter :authenticate_user!, :only => [:new, :create]
 
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+
 	def new
 		@group = Group.find(params[:group_id])
 		@post = Post.new
 	end
 
-	def create
-		@group = Group.find(params[:group_id])
-		@post = Post.new(post_params)
-		@post.group = @group
-		@post.user = current_user
+	# def create
+	# 	@group = Group.find(params[:group_id])
+	# 	@post = Post.new(post_params)
+	# 	@post.group = @group
+	# 	@post.user = current_user
 
-		if @post.save
-			redirect_to group_path(@group)
+	# 	if @post.save
+	# 		redirect_to group_path(@group)
+	# 	else
+	# 		render :ne2w
+	# 	end
+	# end
+
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to posts_path, notice: "Update Success"
 		else
-			render :ne2w
+			render :edit
 		end
 	end
+
 
 private
 
